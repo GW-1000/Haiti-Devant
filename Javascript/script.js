af113bb8da26985cc5a788221e4aee1cd2ebfec6
Thi2erry8 <123498029+Thi2erry8@users.noreply.github.com>
2024-03-25 13:48:31 -0400

    Update script.js

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,2 +1,20 @@
-var copy = document.querySelector(".scroll_items").cloneNode(true);
-document.querySelector(".scroll").appendChild(copy);
+const loginForm = document.querySelector(".login-form");
+const signupForm = document.querySelector(".signup-form");
+const loginBtn = document.querySelector(".login button");
+const signupBtn = document.querySelector(".signup button");
+const backLayer = document.querySelector(".back-layer");
+
+signupBtn.addEventListener("click", () => {
+  loginForm.classList.remove("active");
+  signupForm.classList.add("active");
+  backLayer.style.clipPath = "inset(0 0 0 50%)";
+});
+
+loginBtn.addEventListener("click", () => {
+  signupForm.classList.remove("active");
+  loginForm.classList.add("active");
+  backLayer.style.clipPath = "";
+});
+
+
+
